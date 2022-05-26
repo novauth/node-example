186204b5e4112d453197752bc2f26f73d121b557
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2022-05-26 22:34:18 +0200

    add basic sign up / sign in form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import { Button, Col, Container, Form, Row, Tab, Tabs } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={styles.baseContainer}>
+      <Row className="justify-content-center align-items-center">
+        <Col className={`${styles.centeredContainer}`}>
+          <div className="text-center">
+            <img
+              src="logo512.png"
+              className={`${styles.logo} mb-3`}
+              alt="logo"
+            />
+            <h1>NovAuth Authentication Example</h1>
+            <p className="lead">
+              Push authentication made easy. Test it by yourself.
+            </p>
+          </div>
+          <div className={`p-4 ${styles.formContainer} mb-3`}>
+            <Tabs
+              defaultActiveKey="sign-up"
+              id="uncontrolled-tab-example"
+              className="mb-3"
+            >
+              <Tab eventKey="sign-up" title="Sign Up">
+                <p>
+                  Create a test account here and associate it with the NovAuth
+                  Authenticator app on your phone.
+                </p>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="username"
+                      placeholder="Enter username"
+                    />
+                    <Form.Text className="text-muted">
+                      Whoah, no password? Welcome to the passwordless future. :)
+                    </Form.Text>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Sign Up
+                  </Button>
+                </Form>
+              </Tab>
+              <Tab eventKey="sign-in" title="Sign In">
+                <p>
+                  Provide your username here, then authenticate to the service
+                  by receiving a push notification on the NovAuth Authenticator
+                  app on your phone.
+                </p>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="username"
+                      placeholder="Enter username"
+                    />
+                    <Form.Text className="text-muted">
+                      Remember, your phone is the key to access your account.
+                    </Form.Text>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Sign In
+                  </Button>
+                </Form>
+              </Tab>
+            </Tabs>
+          </div>
+          <p className="text-center">
+            Learn more on{" "}
+            <a href="https://github.com/novauth/novauth">GitHub</a>.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
